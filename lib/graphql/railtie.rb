849aabf87786c60ca0e7c39459b5b5ff20fb7668
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2023-09-22 15:20:51 -0500

    Make GraphQL parser cache opt-in
    The cache has nothing to do with bootsnap, so we don't need to piggyback
    off of it at all. This will prevent bootsnap cache to be polluted by the
    graphql gem, and allow more apps to leverage GQL caching. The parser
    cache is also now opt-in, which will make this behaviour more expected.

--- lib/graphql/railtie.rb
+++ lib/graphql/railtie.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
+
 module GraphQL
   class Railtie < Rails::Railtie
-    config.before_configuration do
-      # Bootsnap compile cache has similar expiration properties,
-      # so we assume that if the user has bootsnap setup it's ok
-      # to piggy back on it.
-      if ::Object.const_defined?("Bootsnap::CompileCache::ISeq") && Bootsnap::CompileCache::ISeq.cache_dir
-        Language::Parser.cache ||= Language::Cache.new(Pathname.new(Bootsnap::CompileCache::ISeq.cache_dir).join('graphql'))
+    config.graphql = ActiveSupport::OrderedOptions.new
+    config.graphql.parser_cache = false
+
+    config.before_initialize do
+      if !config.graphql.parser_cache && ::Object.const_defined?("Bootsnap::CompileCache::ISeq") && Bootsnap::CompileCache::ISeq.cache_dir
+        Deprecation.warn <<~MSG.squish
+          The GraphQL parser cache must be explicitly enabled in your application.
+          Please add `config.graphql.parser_cache = true` to your application config.
+        MSG
+        config.graphql.parser_cache = true
+      end
+    end
+
+    initializer("graphql.cache") do |app|
+      if config.graphql.parser_cache
+        Language::Parser.cache ||= Language::Cache.new(
+          app.root.join("tmp/cache/graphql")
+        )
       end
     end
   end
